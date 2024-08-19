59d29c5aa05b1b6aaa4a5e82624d8b242b059b3b
Alexei Godfray <58841622+AlexeiGodfray@users.noreply.github.com>
2024-08-19 15:30:07 -0700

    Cleaned up the code slightly, implemented Articles 2 and 3

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+def main():
+    npr_info()
+    
+def npr_info():
+    driver.get("https://www.npr.org/sections/news/")
+    Headline1 = driver.find_element("xpath",'//*[@id="featured"]/div/article[1]/div[2]/div/h2/a')
+    Headline2 = driver.find_element("xpath", '//*[@id="featured"]/div/article[2]/div[2]/div/h2/a')
+    Headline3 = driver.find_element("xpath", '//*[@id="featured"]/div/article[3]/div[2]/div/h2/a')
 
+    link1 = Headline1.get_attribute('href')
+    link2 = Headline2.get_attribute('href')
+    link3 = Headline3.get_attribute('href')
+    
+    headline_info = Headline1.text + "\n" + Headline2.text  + "\n" + Headline3.text
+    print(headline_info)
 
-driver.get("https://www.npr.org/sections/news/")
-Headline1 = driver.find_element("xpath",'//*[@id="featured"]/div/article[1]/div[2]/div/h2/a')
-Headline2 = driver.find_element("xpath", '//*[@id="featured"]/div/article[2]/div[2]/div/h2/a')
-
-aTag1 = driver.find_element("xpath", '//*[@id="featured"]/div/article[1]/div[2]/div/h2/a')
-aTag2 = driver.find_element("xpath", )
-link1 = aTag1.get_attribute('href')
-
-
-print(Headline2.text)
-
+main()
 driver.quit()
