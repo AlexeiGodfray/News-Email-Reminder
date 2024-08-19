770f29c958e2b5ab1e47c7ab3cf6318c1fa54bec
Alexei Godfray <58841622+AlexeiGodfray@users.noreply.github.com>
2024-08-19 16:29:04 -0700

    Properly formated NPR function for human readablity, enabled Chrome options, enabled headless mode for speed and customizablity

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+# chrome_options.add_argument("--disable-extensions")
+# chrome_options.add_argument("--disable-gpu")
+# chrome_options.add_argument("--no-sandbox") # linux only
+chrome_options.add_argument("--headless=new") # for Chrome >= 109
+driver = webdriver.Chrome(options=chrome_options)
+
 def main():
     npr_info()
     
@@ -15,7 +22,7 @@ def npr_info():
     link2 = Headline2.get_attribute('href')
     link3 = Headline3.get_attribute('href')
     
-    headline_info = Headline1.text + "\n" + Headline2.text  + "\n" + Headline3.text
+    headline_info = "NPR:"+ "\n" + Headline1.text +"\n" + link1 + "\n" + Headline2.text +"\n" + link2 + "\n" + Headline3.text +"\n" + link3 
     print(headline_info)
 
 main()
