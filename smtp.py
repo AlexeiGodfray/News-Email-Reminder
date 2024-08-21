ba95e1fbab497b02d09f00d89838eb25ccf0a393
Alexei Godfray <58841622+AlexeiGodfray@users.noreply.github.com>
2024-08-21 11:46:13 -0700

    A quick SMTP email automation script, will be implementing it in main.py soon

---
+++ smtp.py
@@ -0,0 +1,37 @@
+import smtplib
+from email.message import EmailMessage
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file for secuirty concerns
+load_dotenv()
+
+from_addr = os.getenv("EMAIL_ADDRESS")
+to_addrs = os.getenv("EMAIL_RECIPIENT")
+email_password = os.getenv("EMAIL_PASSWORD")
+
+def email_logic():
+    # Prepare the message
+    msg = EmailMessage()
+    msg["From"] = from_addr
+    msg["To"] = to_addrs
+    msg["Subject"] = "News of Today"
+
+    # Auto-generate the message content
+    message_lines = "auto populate"
+    msg.set_content("\n".join(message_lines))
+
+    print("Message length is", len(msg.as_string()))
+
+    try:
+        # Connect to Gmail's SMTP server
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()  # Start TLS encryption
+        # Use environment variables or another secure way to store your credentials
+        server.login(from_addr, email_password)  # Gmail authentication
+        server.send_message(msg)
+        print("Email sent successfully!")
+    except smtplib.SMTPException as e:
+        print(f"Error: {e}")
+    finally:
+        server.quit()
